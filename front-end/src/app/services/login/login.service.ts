04f19f8d6580e33e20f7280b0af1a5711009a66e
Micle <miclemattiol@gmail.com>
2022-12-26 17:35:36 +0100

    Hashato la password e assicurato il funzionamento del login

--- front-end/src/app/services/login/login.service.ts
+++ front-end/src/app/services/login/login.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable, Output, EventEmitter } from '@angular/core';
 import { loginUrl, logoutUrl } from 'src/app/shared/routes';
+import * as cryptoJs from 'crypto-js';
 
 @Injectable({
   providedIn: 'root'
@@ -14,6 +15,7 @@ export class LoginService {
   @Output() loginStateChange = new EventEmitter<string | undefined>();
 
   login(email: string, password: string) {
+    const _password = this.hashPassword(password);
     const req = this.httpClient.post(loginUrl, { email, password }).subscribe((res) => {
       req.unsubscribe();
       console.log(res);
@@ -23,8 +25,9 @@ export class LoginService {
   }
 
   //cf, name, surname, birthDate, phoneNumber, email, address, password, subscriptionDate
-  register(cf: string, name: string, surname: string, birthDate: Date, phoneNumber: string, email: string, address: string, password: string, subscriptionDate: Date){
-    const req = this.httpClient.put(loginUrl, {cf, name, surname, birthDate, phoneNumber, email, address, password, subscriptionDate}).subscribe((res) => {
+  register(cf: string, name: string, surname: string, birthDate: Date, phoneNumber: string, email: string, address: string, password: string, subscriptionDate: Date) {
+    const _password = this.hashPassword(password);
+    const req = this.httpClient.put(loginUrl, { cf, name, surname, birthDate, phoneNumber, email, address, password, subscriptionDate }).subscribe((res) => {
       req.unsubscribe();
       console.log(res);
       this.token = (res as any).token;
@@ -36,5 +39,9 @@ export class LoginService {
     this.httpClient.post(logoutUrl, { token: this.token });
     this.token = undefined;
     this.loginStateChange.emit(this.token);
+  }
+
+  hashPassword(password: string) {
+    return CryptoJS.SHA256(password).toString();
   }
 }
